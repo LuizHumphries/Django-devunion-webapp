cc56978fa2e117310a2cff1bdd321e605aa98aaf
Luiz Humphries <luizgustavo.frota@outlook.com>
2024-03-27 20:13:28 -0300

    feat: Search logic added. CRUD for Projects, Profile, Skills added

--- users/signals.py
+++ users/signals.py
@@ -7,13 +7,23 @@ from .models import Profile
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user = instance
-        profile = Profile.objects.create(
+        print('ok')
+        Profile.objects.create(
             user=user,
             username = user.username,
             email=user.email,
             name=user.first_name
         )
 
+def update_user(sender, instance, created, **kwargs):    
+    profile = instance
+    user = profile.user
+    if not created:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+        
 
 #@receiver(post_delete, sender=Profile)
 def user_delete(sender, instance, **kwargs):
@@ -21,4 +31,5 @@ def user_delete(sender, instance, **kwargs):
     user.delete()
 
 post_save.connect(create_profile, sender=User)
+post_save.connect(update_user, sender=Profile)
 post_delete.connect(user_delete, sender=Profile)
