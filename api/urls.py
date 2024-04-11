e99dbf69d0c57d48866c11d4582fab4b89a1e334
Luiz Humphries <luizgustavo.frota@outlook.com>
2024-04-10 22:39:20 -0300

    feat: REST API get projects, project (single), token, refresh token, vote

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,15 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_routes)
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('', views.get_routes),
+    path('projects/', views.get_projects),
+    path('projects/<str:pk>/', views.get_project),
+    path('projects/<str:pk>/vote', views.project_vote),
 ]
+
+
--- api/views.py
+++ api/views.py
@@ -1,5 +1,10 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from api.serializers import ProjectSerializers
+from projects.models import Project, Review
 
+@api_view(["GET"])
 def get_routes(request):
 
     routes = [
@@ -9,5 +14,39 @@ def get_routes(request):
         {"POST": '/api/users/token'},
         {"POST": '/api/users/token/refresh'},
     ]
+    return Response(routes)
 
-    return JsonResponse(routes, safe=False)
+@api_view(["GET"])
+#@permission_classes([IsAuthenticated])
+def get_projects(request):
+    #print("USER:", request.user)
+    projects = Project.objects.all()
+    serializer = ProjectSerializers(projects, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+#@permission_classes([IsAuthenticated])
+def get_project(request, pk):
+    projects = Project.objects.get(id=pk)
+    serializer = ProjectSerializers(projects, many=False)
+    return Response(serializer.data)
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def project_vote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project,
+    )
+
+    review.value = data['value']
+    review.save()
+    project.get_vote_count
+    
+    print("DATA:", data)
+    serializer = ProjectSerializers(project, many=False)
+    return Response(serializer.data)
